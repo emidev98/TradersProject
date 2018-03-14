0d7b801eea17ee69b92294155472ae527c8201ba
Marc Sala Manzano <Marc>
2018-03-14 19:53:32 +0100

    SolarSystem with planet list

--- traders/src/main/java/model/SolarSystem.java
+++ traders/src/main/java/model/SolarSystem.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.Session;
@@ -31,6 +33,9 @@ public class SolarSystem {
 	
 	@Column(name = "Name")
 	private String name;
+	
+	@OneToMany(mappedBy="systemId", cascade={CascadeType.ALL})
+	private List<Planet> planets;
 	
 	public SolarSystem() {
 		;
@@ -59,12 +64,20 @@ public class SolarSystem {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public List<Planet> getPlanets() {
+		return planets;
+	}
+
+	public void setPlanets(List<Planet> planets) {
+		this.planets = planets;
+	}
 
 	@Override
 	public String toString() {
-		return "SolarSystem [id=" + id + ", code=" + code + ", name=" + name + "]";
+		return "SolarSystem [id=" + id + ", code=" + code + ", name=" + name + ", planets=" + planets + "]";
 	}
-	
+
 	public static List<SolarSystem> getAllSolarSystems() {
 		SessionFactory factory = HibernateUtil.getInstance().getSessionFactory();
 		Session session = factory.getCurrentSession();
