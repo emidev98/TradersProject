9006c51ab78eaebd96ec1c43b1d6fc29a2399297
Marc Sala Manzano <Marc>
2018-03-14 19:22:22 +0100

    Feature ??

---
+++ traders/src/main/java/model/SolarSystems.java
@@ -0,0 +1,77 @@
+package model;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import dao.HibernateUtil;
+
+@ManagedBean
+@SessionScoped
+@Entity
+@Table(name = "SolarSystems")
+public class SolarSystems {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "Id")
+	private int id;
+	
+	@Column(name = "Code")
+	private String code;
+	
+	@Column(name = "Name")
+	private String name;
+	
+	public SolarSystems() {
+		;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "SolarSystem [id=" + id + ", code=" + code + ", name=" + name + "]";
+	}
+	
+	public List<SolarSystems> getAllSolarSystems() {
+		SessionFactory factory = HibernateUtil.getInstance().getSessionFactory();
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		List<SolarSystems> solarSystems = session.createQuery("from SolarSystems").getResultList();
+		session.getTransaction().commit();
+		return solarSystems;
+	}
+	
+}
