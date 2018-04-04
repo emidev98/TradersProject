902e9a1f00ff72122a22510dd9f9eaccc73a4158
Emi Harastasan <eharastasan@gmail.com>
2018-04-05 01:48:08 +0200

    Feature_chose-planet

--- traders/src/main/java/controller/ChosePlanet.java
+++ traders/src/main/java/controller/ChosePlanet.java
@@ -1,9 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import model.Planet;
 import model.SolarSystem;
@@ -13,11 +14,41 @@ import model.SolarSystem;
 public class ChosePlanet {
 	
 	private List<SolarSystem> solarSystems = null;
-	private List<Planet> planets = null;
+	private List<Planet> planets = new ArrayList<Planet>();
 	private SolarSystem selectedSolarSystem = null;
 	private Planet selectedPlanet = null;
 	
+	public ChosePlanet() {
+		solarSystems = SolarSystem.getAllSolarSystems();
+	}
+	
+	public void setSolarSystem(AjaxBehaviorEvent e) {
+		System.out.println("selectedSolarSystem " + selectedSolarSystem);
+	}
+	
+	public void setSelectedSolarSystem(SolarSystem selectedSolarSystem) {
+		this.selectedSolarSystem = selectedSolarSystem;
+	}
 
+	public void retrivePlanets(AjaxBehaviorEvent e) {
+		System.out.println("SelectedSolarSystem-> " + selectedSolarSystem);
+		if(selectedSolarSystem != null)
+			this.planets = selectedSolarSystem.getPlanets();
+		System.out.println("planets" + planets);
+	}
+	
+	public List<Planet> getPlanets(){
+		return planets;
+	}
+	
+	public List<SolarSystem> getSolarSystems() {
+		return solarSystems;
+	}
+	
+	public SolarSystem getSelectedSolarSystem() {
+		return selectedSolarSystem;
+	}
+	
 	public Planet getSelectedPlanet() {
 		return selectedPlanet;
 	}
@@ -25,38 +56,4 @@ public class ChosePlanet {
 	public void setSelectedPlanet(Planet selectedPlanet) {
 		this.selectedPlanet = selectedPlanet;
 	}
-
-	public ChosePlanet() {
-		solarSystems = SolarSystem.getAllSolarSystems();
-		System.out.println(solarSystems);
-	}
-	
-	public void setSolarSystems(List<SolarSystem> solarSystems) {
-		this.solarSystems = solarSystems;
-	}
-
-	public void setSelectedSolarSystem(SolarSystem selectedSolarSystem) {
-		this.selectedSolarSystem = selectedSolarSystem;
-	}
-
-	public List<SolarSystem> getSolarSystems() {
-		return solarSystems;
-	}
-	
-	public List<Planet> getPlanets(SolarSystem solarSystem) {
-		return solarSystem.getPlanets();
-	}
-	
-	public List<Planet> getPlanets() {
-		return planets;
-	}
-	
-	public void setPlanets(List<Planet> planets) {
-		this.planets = planets;
-	}
-
-	public SolarSystem getSelectedSolarSystem() {
-		return selectedSolarSystem;
-	}
-	
 }
