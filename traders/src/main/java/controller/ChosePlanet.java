5ee54ce2a55758305286b0b31a42e3366102d7d6
Emi Harastasan <eharastasan@gmail.com>
2018-04-11 19:19:01 +0200

    Feature_change-scope

--- traders/src/main/java/controller/ChosePlanet.java
+++ traders/src/main/java/controller/ChosePlanet.java
@@ -3,13 +3,13 @@ package controller;
 import java.io.Serializable;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.event.AjaxBehaviorEvent;
 import model.Planet;
 import model.SolarSystem;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ChosePlanet implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
--- traders/src/main/java/controller/CreateTrader.java
+++ traders/src/main/java/controller/CreateTrader.java
@@ -2,12 +2,14 @@ package controller;
 
 import java.sql.SQLException;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import model.Trader;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class CreateTrader {
+	
+	private Trader trader;
 	
 	public CreateTrader() {
 
@@ -15,12 +17,21 @@ public class CreateTrader {
 		
 	public String create(Trader trader) {
 		try {
+			this.trader = trader;
 			trader.saveTrader();	
 		} catch(SQLException e) {
 			
 		}
 			
 		return "choseplanet.xhtml";
+	}
+
+	public Trader getTrader() {
+		return trader;
+	}
+
+	public void setTrader(Trader trader) {
+		this.trader = trader;
 	}
 	
 }
