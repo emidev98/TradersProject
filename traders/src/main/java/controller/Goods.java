f11cece48a904d9eeec0b9def2490bb9000f227c
Emi Harastasan <eharastasan@gmail.com>
2018-04-23 21:45:08 +0200

    Feature_goods

--- traders/src/main/java/controller/Goods.java
+++ traders/src/main/java/controller/Goods.java
@@ -1,13 +1,48 @@
 package controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import model.Good;
 
 @ManagedBean
 @SessionScoped
 public class Goods implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	private List<Good> goods;
+	private Good good;
+	private String goodId;
+	
+	public void onGoodChangeListener(AjaxBehaviorEvent event) {
+		System.out.println("onListener -> " + goodId);
+	}
+	
+	//Getters, setters and constructor
+	public Goods() {
+		goods = Good.getAllGoods();
+	}
+
+	public List<Good> getGoods() {
+		return goods;
+	}
+
+	public void setGoods(List<Good> goods) {
+		this.goods = goods;
+	}
+
+	public Good getGood() {
+		return good;
+	}
+
+	public void setGood(String goodId) {
+		this.goodId = goodId;
+		System.out.println("good -> " + goodId);
+	}
+
 	
 }
--- traders/src/main/java/model/Good.java
+++ traders/src/main/java/model/Good.java
@@ -69,6 +69,17 @@ public class Good {
 		}
 		return prices;
 	}
+	
+	public static List<Good> getAllGoods() {
+		SessionFactory factory = HibernateUtil.getInstance().getSessionFactory();
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		@SuppressWarnings("unchecked")
+		List<Good> goods = session.createQuery("from Good").getResultList();
+		session.getTransaction().commit();
+		return goods;
+	}
+	
 	/*
 	 * SELECT *
 	 * 	FROM PriceChanges pc
